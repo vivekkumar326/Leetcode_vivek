f30a134c8ee5ce11470f931cb600bf1039d6d68a
vivek kumar <130165788+vivekkumar326@users.noreply.github.com>
2026-06-30 07:26:28 +0530

    Time: 0 ms (100%), Space: 10 MB (86.57%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+   int romanToInt(string s) {
+    int res = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        int curr = 0, next = 0;
+
+        switch (s[i]) {
+            case 'I': curr = 1; break;
+            case 'V': curr = 5; break;
+            case 'X': curr = 10; break;
+            case 'L': curr = 50; break;
+            case 'C': curr = 100; break;
+            case 'D': curr = 500; break;
+            case 'M': curr = 1000; break;
+        }
+
+        if (i + 1 < s.length()) {
+            switch (s[i + 1]) {
+                case 'I': next = 1; break;
+                case 'V': next = 5; break;
+                case 'X': next = 10; break;
+                case 'L': next = 50; break;
+                case 'C': next = 100; break;
+                case 'D': next = 500; break;
+                case 'M': next = 1000; break;
+            }
+        }
+
+        if (curr < next) res -= curr;
+        else res += curr;
+    }
+
+    return res;
+   }
+};
